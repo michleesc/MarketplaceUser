1954abe9ecee66d96df57e2c5c9069d5cc47222a
michleesc <michlee.septian.c@gmail.com>
2023-11-08 16:11:06 +0700

    Penerapan restful api menggunakan swagger

--- src/main/java/comp/finalproject/user/MarketplaceUser.java
+++ src/main/java/comp/finalproject/user/MarketplaceUser.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-//@EnableWebMvc
 @SpringBootApplication
 @ComponentScan(basePackages = {"comp.finalproject.user"})
 public class MarketplaceUser {
