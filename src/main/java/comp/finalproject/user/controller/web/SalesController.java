c788c3b202035a7f38c38e8f56ecfeaa077c6cea
michleesc <michlee.septian.c@gmail.com>
2023-11-08 11:48:09 +0700

    perbaikan pada Controller web

--- src/main/java/comp/finalproject/user/controller/web/SalesController.java
+++ src/main/java/comp/finalproject/user/controller/web/SalesController.java
@@ -10,11 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.security.Principal;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 @Controller
